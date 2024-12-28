7ad216c72622e0ebc3ed334d1b33415d6ebce766
rehan123coder <rehankhann@gmail.com>
2024-12-28 12:54:50 +0500

    op

---
+++ ar.py
@@ -0,0 +1,9 @@
+a=15
+b=20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(b%a)
+print(a**b)
---
+++ dm.py
@@ -0,0 +1,4 @@
+amount=int(input("enter your amount"))
+note100=amount//100
+note50=(amount%100)//50
+print(note100)
