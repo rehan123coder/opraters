41685865a8f753c1f74e88e3279d931446621bc8
rehan123coder <rehankhann@gmail.com>
2025-02-10 19:04:38 +0500

    nested

---
+++ op.py
@@ -0,0 +1,14 @@
+a=11
+b=11.0
+c=11.1
+d=11.2
+ans=(a-b)**(c+d)
+print(ans)
+#no is divisiible or not
+nume=int(input("enter the numerator"))
+deno=int(input("enter the denominator"))
+if nume%deno==0:
+    print("this number is divisible")
+else:
+    print("this number is not divisible")
+    
