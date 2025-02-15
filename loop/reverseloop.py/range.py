8f71ed542ae9b1d02c94d68fe26c777ee245a05e
rehan123coder <rehankhann@gmail.com>
2025-02-15 13:31:34 +0500

    range

---
+++ loop/reverseloop.py/range.py
@@ -0,0 +1,6 @@
+n=int(input("Ente the number hhose sum you wan to find:" ))
+sum=0
+for n+1 times : i=1 to n+1
+for i in range(1, n+1):
+    sum=sum+i
+    print("\nSum =", sum)
---
+++ loop/reverseloop.py/revers.py
@@ -0,0 +1,5 @@
+string1=input("Please enter your own string:")
+string2=('')
+for i in string1:
+    string2=i+string2
+print(string2)
